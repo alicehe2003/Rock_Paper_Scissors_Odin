e42d774741014e851155920e63a5c686cc955564
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-05-12 19:02:07 -0700

    Implemented getHumanChoice. Takes valid user input and returns it.

--- helloWorld.js
+++ helloWorld.js
@@ -9,3 +9,13 @@ function getComputerChoice() {
     }
 }
 
+// returns user choice, -1 if invalid
+function getHumanChoice() {
+    let choice = prompt("Rock, paper, or scissors?");
+    if (choice == "rock" || choice == "paper" || choice == "scissors") {
+        return choice; 
+    } else {
+        return -1; 
+    }
+}
+
