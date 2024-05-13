5a313622a07bedb5446e35d89ad3fa69ebf14e73
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-05-12 20:14:57 -0700

    Implemented logic to play a single round.

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,4 @@
+// randomly generates one of "rock", "paper", or "scissors"
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3)
     if (num == 0) {
@@ -9,13 +10,44 @@ function getComputerChoice() {
     }
 }
 
-// returns user choice, -1 if invalid
+// returns valid user choice, default to rock 
 function getHumanChoice() {
-    let choice = prompt("Rock, paper, or scissors?");
+    let choice = prompt("Rock, paper, or scissors?").toLowerCase();
     if (choice == "rock" || choice == "paper" || choice == "scissors") {
         return choice; 
     } else {
-        return -1; 
+        return "rock"; 
     }
 }
+
+let humanScore = 0; 
+let computerScore = 0; 
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock") {
+        if (computerChoice == "scissors") {
+            ++humanScore; 
+        } else if (computerChoice == "paper") {
+            ++computerScore; 
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            ++humanScore; 
+        } else if (computerChoice == "scissors") {
+            ++computerScore; 
+        }
+    } else {
+        if (computerChoice == "rock") {
+            ++computerScore; 
+        } else if (computerChoice == "paper") {
+            ++humanScore; 
+        }
+    }
+} 
+
+const humanSelection = getHumanChoice(); 
+const computerSelection = getComputerChoice(); 
+
+playRound(humanSelection, computerSelection); 
+
 
