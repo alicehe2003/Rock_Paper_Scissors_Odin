6ae4a9540c19e0d730099e0224932c12c77b4355
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-05-12 18:54:48 -0700

    Implemented getComputerChoice function. Generates one of "rock", "paper", "scissors" randomly.

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
-console.log("Hello World"); 
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3)
+    if (num == 0) {
+        return "rock"; 
+    } else if (num == 1) {
+        return "paper"; 
+    } else {
+        return "scissors"; 
+    }
+}
+
